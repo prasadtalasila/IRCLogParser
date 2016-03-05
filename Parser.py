dbe3c6c0d7fb01f32220a7dd4844db6baa47fa1e
Rohan Goel <rohangoel0296@gmail.com>
2016-03-05 17:17:30 +0530

    Library Functions Implemented

---
+++ Parser.py
@@ -0,0 +1,20 @@
+import lib.NickChangeGraph as NCG 
+import lib.MessageTimeGraph as MTG
+import lib.MessageNumberGraph as MNG
+import lib.MessageNumberBinsCSV as MNBC
+import lib.AggregateGraph as AG
+
+def createNickChangesGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth):
+	NCG.createNickChangesGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth)
+
+def createMessageTimeGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth):
+	MTG.createMessageTimeGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth)
+
+def createMessageNumberGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth):
+	MNG.createMessageNumberGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth)
+
+def createMessageNumberBinsCSV(log_directory, channel_name, output_directory, startingMonth, endingMonth):
+	MNBC.createMessageNumberBinsCSV(log_directory, channel_name, output_directory, startingMonth, endingMonth)
+	
+def createAggregateGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth):
+	AG.createAggregateGraph(log_directory, channel_name, output_directory, startingMonth, endingMonth)
---
+++ README.md
@@ -0,0 +1,1 @@
+#IRC Log Parser
