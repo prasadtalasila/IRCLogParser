f177844bf2f40d57b3986bebc1e9ad9c796c9226
joey <dhruvbits315@gmail.com>
2016-07-08 12:19:10 +0530

    First commit

--- TestingScript.py
+++ TestingScript.py
@@ -1,12 +1,12 @@
 import Parser
 
-log_directory = "/home/rohan/parser_files/2013/"
+log_directory = "/home/dhruvie/LOP/2013/"
 channel_name= "#kubuntu-devel" #channel name
-output_directory = "/home/rohan/parser_files/Output/"
+output_directory = "/home/dhruvie/LOP/Output/"
 startingDate = 1
 startingMonth = 1
-endingDate = 1
-endingMonth = 2
+endingDate = 10
+endingMonth = 1
 
 '''
 	TRY LARGE TIME RANGES ON THE FOLLOWING GRAPHS WITH UTMOST CARE
@@ -14,12 +14,16 @@ endingMonth = 2
 '''
 
 # Parser.createNickChangesGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
-# Parser.createMessageTimeGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
-# Parser.createMessageNumberGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+Parser.createMessageTimeGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.createMessageNumberGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
 # Parser.createMessageNumberBinsCSV(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
-# Parser.createAggregateGraph(log_directory, channel_name, startingDate, startingMonth, endingDate, endingMonth)
-Parser.createChannelsOfNickGraph(log_directory, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.createAggregateGraph(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+# Parser.createChannelsOfNickGraph(log_directory, output_directory, startingDate, startingMonth, endingDate, endingMonth)
 # Parser.createKeyWords(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
 # Parser.degreeMessageNumberCSV(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
 # Parser.degreeNodeNumberCSV(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
 # Parser.svdOnKeywords(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.findResponseTime(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.findConvLength_ConvRefreshTime(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.createGephiTimelapseCSV(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
+#Parser.implementWithIgraphs(log_directory, channel_name, output_directory, startingDate, startingMonth, endingDate, endingMonth)
