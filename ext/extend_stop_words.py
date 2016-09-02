bc113bafd0a0e88d11266922d6af13c716f29da4
Rohan Goel <rohangoel0296@gmail.com>
2016-09-02 20:00:00 +0530

    update keywords to limit min length=3 and ignore numbers in keywords

--- ext/extend_stop_words.py
+++ ext/extend_stop_words.py
@@ -5,5 +5,5 @@
 	Use the function to concert them all : words = map(lambda x:x.lower(),words) 
 '''
 
-words = ['fail', 'connect', 'failed', 'infact', 'later', 'guys']
-slangs = ['btw', 'ok']
+words = ['fail', 'connect', 'failed', 'infact', 'later', 'guys', 'jesus', 'monday', 'tuesday', 'wednesday' ,'thursday', 'friday', 'saturday', 'sunday']
+slangs = ['btw', 'ok', '2f4u', '4yeo', 'aamof', 'ack', 'afaik', 'afair', 'afk', 'aka', 'b2k', 'btt', 'btw', 'b/c', 'c&p', 'cu', 'cys', 'diy', 'eobd', 'eod', 'eom', 'eot', 'faq', 'fack', 'fka', 'fwiw', 'fyi', 'ftw', 'hf', 'hth', 'idk', 'iirc', 'imho', 'imo', 'imnsho', 'iow', 'itt', 'lol', 'mmw', 'n/a', 'nan', 'nntr', 'noob', 'noyb', 'nrn', 'omg', 'op', 'ot', 'otoh', 'pebkac', 'pov', 'rotfl', 'rsvp', 'rtfm', 'scnr', 'sflr', 'spoc', 'tba', 'tbc', 'tia', 'tgif', 'thx', 'tq', 'tyvm', 'tyt', 'ttyl', 'woot', 'wfm', 'wrt', 'wth', 'wtf', 'ymmd', 'ymmv', 'yam', 'hehe']
