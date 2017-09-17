76ce5657a7d118ac59a5e577d9814215fcb3fc69
prasadtalasila <tsrkp@goa.bits-pilani.ac.in>
2017-09-17 18:06:47 +0530

    refactor unit tests for better code quality

--- test/lib/UtilTest.py
+++ test/lib/UtilTest.py
@@ -1,25 +1,28 @@
 import unittest
-import pickle
 import lib.util as util
 import commands
 import subprocess
 
 class UtilTest(unittest.TestCase):
 
-    @staticmethod
-    def test_save_to_disk():
-        nicks = ['BluesKaj', 'Peace-', 'LordOfTime', 'TheLordOfTime', 'benonsoftware', 'Benny', 'Guest43293', 'rdieter_work', 'rdieter', 'jjesse-home_', 'jjesse-home', 'rdieter1', 'Mamarok', 'Mamarok_', 'G4MBY', 'PaulW2U', 'jussi', 'shadeslayer', 'Tm_T', 'yofel', 'ScottK', 'Quintasan', 'mikhas', 'ubottu', 'Noskcaj', 'Riddell', 'Tonio_', 'Tonio_aw', 'yofel_', 'Quintasan_']
-        util.save_to_disk(nicks,"data/nicksTest")
+    def setUp(self):
+        self.nicks = ['BluesKaj', 'Peace-', 'LordOfTime', 'TheLordOfTime', 'benonsoftware', 'Benny', 'Guest43293', 'rdieter_work', 'rdieter', 'jjesse-home_', 'jjesse-home', 'rdieter1', 'Mamarok', 'Mamarok_', 'G4MBY', 'PaulW2U', 'jussi', 'shadeslayer', 'Tm_T', 'yofel', 'ScottK', 'Quintasan', 'mikhas', 'ubottu', 'Noskcaj', 'Riddell', 'Tonio_', 'Tonio_aw', 'yofel_', 'Quintasan_']
+        self.expected_nicks = self.nicks
+
+    def tearDown(self):
+        self.nicks = None
+        self.expected_nicks = None
+
+    def test_save_to_disk(self):
+        util.save_to_disk(self.nicks,"data/nicksTest")
         status, output = commands.getstatusoutput('cmp data/nicks data/nicksTest')
         subprocess.Popen(['rm', 'data/nicksTest'])
         assert status == 0, "Failure to load from disk."
 
 
-    @staticmethod
-    def test_load_from_disk():
-        expected_nicks = ['BluesKaj', 'Peace-', 'LordOfTime', 'TheLordOfTime', 'benonsoftware', 'Benny', 'Guest43293', 'rdieter_work', 'rdieter', 'jjesse-home_', 'jjesse-home', 'rdieter1', 'Mamarok', 'Mamarok_', 'G4MBY', 'PaulW2U', 'jussi', 'shadeslayer', 'Tm_T', 'yofel', 'ScottK', 'Quintasan', 'mikhas', 'ubottu', 'Noskcaj', 'Riddell', 'Tonio_', 'Tonio_aw', 'yofel_', 'Quintasan_']
+    def test_load_from_disk(self):
         nicks = util.load_from_disk("data/nicks")
-        assert nicks == expected_nicks, "Failure to load from disk."
+        assert nicks == self.expected_nicks, "Failure to load from disk."
 
 
 
--- test/lib/analysis/ChannelTest.py
+++ test/lib/analysis/ChannelTest.py
@@ -4,19 +4,25 @@ import lib.util as util
 
 class ChannelTest(unittest.TestCase):
 
-    @staticmethod
-    def test_conv_len_conv_refr_time():
-        log_data = util.load_from_disk("data/log_data")
-        nicks = util.load_from_disk("data/nicks")
-        nick_same_list = util.load_from_disk("data/nick_same_list")
+    def setUp(self):
+        self.log_data = util.load_from_disk("data/log_data")
+        self.nicks = util.load_from_disk("data/nicks")
+        self.nick_same_list = util.load_from_disk("data/nick_same_list")
+
+    def tearDown(self):
+        self.log_data = None
+        self.nicks = None
+        self.nick_same_list = None
+
+    def test_conv_len_conv_refr_time(self):
         rt_cutoff_time = 1
         cutoff_percentile = 0
         expected_conv_len = util.load_from_disk("data/conv_len")
         expected_conv_ref_time = util.load_from_disk("data/conv_ref_time")
 
         conv_len, conv_ref_time = \
-            channel.conv_len_conv_refr_time(log_data, nicks,
-                                            nick_same_list, rt_cutoff_time, cutoff_percentile)
+            channel.conv_len_conv_refr_time(self.log_data, self.nicks, self.nick_same_list,
+                                            rt_cutoff_time, cutoff_percentile)
 
         assert conv_len == expected_conv_len, \
             "Error in computing conversation length correctly."
@@ -24,18 +30,14 @@ class ChannelTest(unittest.TestCase):
                 "Error in computing conversation refresh time correctly."
 
 
-    @staticmethod
-    def test_response_time():
+    def test_response_time_low_cutoff_percentile(self):
         # this test assumes config.CUTOFF_TIME_STRATEGY = "TWO_SIGMA"
-        log_data = util.load_from_disk("data/log_data")
-        nicks = util.load_from_disk("data/nicks")
-        nick_same_list = util.load_from_disk("data/nick_same_list")
 
         cutoff_percentile = 0.0
         expected_resp_time = util.load_from_disk("data/resp_time")
         expected_cutoff_time = 1
-        resp_time, cutoff_time = channel.response_time(log_data, nicks,
-                                          nick_same_list, cutoff_percentile)
+        resp_time, cutoff_time = channel.response_time(self.log_data, self.nicks,
+                                          self.nick_same_list, cutoff_percentile)
         assert resp_time == expected_resp_time, \
                 "Error in computing response time with 0% cutoff percentile."
         assert cutoff_time == expected_cutoff_time, \
@@ -44,19 +46,22 @@ class ChannelTest(unittest.TestCase):
         cutoff_percentile = 1.0
         expected_resp_time = util.load_from_disk("data/truncated_rt_1percent")
         expected_cutoff_time = 1
-        resp_time, cutoff_time = channel.response_time(log_data, nicks,
-                                          nick_same_list, cutoff_percentile)
+        resp_time, cutoff_time = channel.response_time(self.log_data, self.nicks,
+                                          self.nick_same_list, cutoff_percentile)
         assert resp_time == expected_resp_time, \
                 "Error in computing response time with 1% cutoff percentile."
         assert cutoff_time == expected_cutoff_time, \
                 "Error in computing RT cutoff with 1% cutoff percentile."
 
 
+    def test_response_time_high_cutoff_percentile(self):
+        # this test assumes config.CUTOFF_TIME_STRATEGY = "TWO_SIGMA"
+
         cutoff_percentile = 5.0
         expected_resp_time = util.load_from_disk("data/truncated_rt_5percent")
         expected_cutoff_time = 2
-        resp_time, cutoff_time = channel.response_time(log_data, nicks,
-                                          nick_same_list, cutoff_percentile)
+        resp_time, cutoff_time = channel.response_time(self.log_data, self.nicks,
+                                          self.nick_same_list, cutoff_percentile)
         assert resp_time == expected_resp_time, \
                 "Error in computing response time with 5% cutoff percentile."
         assert cutoff_time == expected_cutoff_time, \
@@ -65,8 +70,8 @@ class ChannelTest(unittest.TestCase):
         cutoff_percentile = 10.0
         expected_resp_time = util.load_from_disk("data/truncated_rt_10percent")
         expected_cutoff_time = 3
-        resp_time, cutoff_time = channel.response_time(log_data, nicks,
-                                          nick_same_list, cutoff_percentile)
+        resp_time, cutoff_time = channel.response_time(self.log_data, self.nicks,
+                                          self.nick_same_list, cutoff_percentile)
         assert resp_time == expected_resp_time, \
                 "Error in computing response time with 10% cutoff percentile."
         assert cutoff_time == expected_cutoff_time, \
