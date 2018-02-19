31d2c62c5a9b34e15529628eb9de4aef59ca5f25
bhuvangupta008 <bhuvangupta459@gmail.com>
2018-02-19 06:52:45 +0530

    Modifies community.py to return None when there are no edge sequences present (#267)

--- lib/analysis/community.py
+++ lib/analysis/community.py
@@ -1,9 +1,5 @@
-import networkx as nx
-import numpy as np
 import igraph
-import sys
 import lib.config as config
-import lib.vis as vis
 
 
 def infomap_igraph(ig_graph, net_file_location=None):
@@ -13,7 +9,6 @@ def infomap_igraph(ig_graph, net_file_location=None):
     Args:
         ig_graph(object): igraph graph object
         net_file_location(str): location to load graph from if not mentioned in ig_graph
-        reduce_graph(bool): toggle between enable/disable reduction
 
     Returns:
         ig_graph: igraph object
@@ -22,7 +17,6 @@ def infomap_igraph(ig_graph, net_file_location=None):
 
     if ig_graph is None:
         # give an option of loading a graph from .net file
-        ig_graph = igraph.Graph()
         ig_graph = igraph.read(net_file_location, format="pajek")
 
     if ig_graph.es:
@@ -36,9 +30,9 @@ def infomap_igraph(ig_graph, net_file_location=None):
         if config.DEBUGGER:
             for node in ig_graph.vs():
                 print str(node.index)+"\t"+str(ig_graph.vs["id"][node.index])
-                # print str(node.index)+"\t"+str(id_to_name_map[g.vs["id"][node.index]])
 
         # http://stackoverflow.com/questions/21976889/plotting-communities-with-python-igraph
         return ig_graph, community
 
-    return ig_graph, None
+    else:
+        return ig_graph, None
