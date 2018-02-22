87cd82fd09589671fbbd65634597db240bbd3986
Bhuvan Gupta <bhuvangupta459@gmail.com>
2018-02-22 19:47:42 +0530

    Adds integration test for vis and modifies unit tests for vis

--- test/unit-test/test_lib/test_vis.py
+++ test/unit-test/test_lib/test_vis.py
@@ -10,16 +10,6 @@ from mock import patch
 from lib import vis, util
 
 
-def mock_exponential_curve_func(x, a, b, c):
-    return a * np.exp(-b * x) + c
-
-
-def mock_generate_probability_distribution(data):
-    topRows = [int(x[1]) for x in data]
-    total = sum(topRows)
-    freq = [x/float(total) for x in topRows]
-    return range(0, len(data)), freq
-
 @ddt
 class VisTest(unittest.TestCase):
     def setUp(self):
@@ -28,6 +18,15 @@ class VisTest(unittest.TestCase):
 
     def tearDown(self):
         self.test_data_dir = None
+
+    def mock_exponential_curve_func(self,x, a, b, c):
+        return a * np.exp(-b * x) + c
+
+    def mock_generate_probability_distribution(self, data):
+        topRows = [int(x[1]) for x in data]
+        total = sum(topRows)
+        freq = [x / float(total) for x in topRows]
+        return range(0, len(data)), freq
 
     @data(([0, 1, 2, 3, 4, 5], [0, 1, 2, 3, 4, 5], [1, 0, 1, 0]))
     @unpack
@@ -78,70 +77,71 @@ class VisTest(unittest.TestCase):
         self.assertEqual(freq, -1)
 
     @patch("lib.vis.generate_probability_distribution", autospec=True)
-    @patch("lib.util.exponential_curve_func", autospec = True)
+    @patch("lib.util.exponential_curve_func", autospec=True)
     @patch("lib.vis.curve_fit", autospec=True)
     def test_exponential_curve_fit_and_plot(self, mock_curve_fit, mock_curve_func, mock_probability_distribution):
         data = util.load_from_disk(self.test_data_dir + "/vis/conv_len")
         expected_result = util.load_from_disk(self.test_data_dir + "/vis/conv_len_fit")
-        mock_curve_fit.return_value = util.load_from_disk(self.test_data_dir+ "/vis/curve_fit")
-        mock_curve_func.side_effect = mock_exponential_curve_func
-        mock_probability_distribution.side_effect = mock_generate_probability_distribution
+        mock_curve_fit.return_value = util.load_from_disk(self.test_data_dir + "/vis/curve_fit")
+        mock_curve_func.side_effect = self.mock_exponential_curve_func
+        mock_probability_distribution.side_effect = self.mock_generate_probability_distribution
 
         output = vis.exponential_curve_fit_and_plot(data, self.test_data_dir, "exponential_plot_test")
         os.remove(self.test_data_dir + '/exponential_plot_test.png')
         assert np.allclose(output, expected_result)
 
     @patch("lib.vis.generate_probability_distribution", autospec=True)
-    @patch("lib.util.exponential_curve_func", autospec = True)
+    @patch("lib.util.exponential_curve_func", autospec=True)
     @patch("lib.vis.curve_fit", autospec=True)
-    def test_exponential_curve_fit_and_plot_x_shifted(self, mock_curve_fit, mock_curve_func, mock_probability_distribution):
+    def test_exponential_curve_fit_and_plot_x_shifted(self, mock_curve_fit, mock_curve_func,
+                                                      mock_probability_distribution):
         data = util.load_from_disk(self.test_data_dir + "/vis/conv_ref_time")
         expected_result = util.load_from_disk(self.test_data_dir + "/vis/conv_ref_time_fit_params")
         mock_curve_fit.return_value = util.load_from_disk(self.test_data_dir + "/vis/curve_fit_x_shifted")
-        mock_curve_func.side_effect = mock_exponential_curve_func
-        mock_probability_distribution.side_effect = mock_generate_probability_distribution
+        mock_curve_func.side_effect = self.mock_exponential_curve_func
+        mock_probability_distribution.side_effect = self.mock_generate_probability_distribution
 
         output = vis.exponential_curve_fit_and_plot_x_shifted(data, self.test_data_dir, "exponential_plot_test_shifted")
         # delete the plot created
         os.remove(self.test_data_dir + '/exponential_plot_test_shifted.png')
         assert np.allclose(output, expected_result)
-    
-    @patch("lib.config.DEBUGGER",1)
-    @patch("igraph.plot",autospec=True)
-    def test_plot_infomap_igraph_no_edges(self,mock_igraph_plot):
-        message_graph = util.load_from_disk(self.test_data_dir+'/vis/message_graph')
-        membership = util.load_from_disk(self.test_data_dir+'/vis/membership')
-        igraph = util.load_from_disk(self.test_data_dir+'/vis/igraph')
-        
+
+    @patch("lib.config.DEBUGGER", 1)
+    @patch("igraph.plot", autospec=True)
+    def test_plot_infomap_igraph_no_edges(self, mock_igraph_plot):
+        message_graph = util.load_from_disk(self.test_data_dir + '/vis/message_graph')
+        membership = util.load_from_disk(self.test_data_dir + '/vis/membership')
+        igraph = util.load_from_disk(self.test_data_dir + '/vis/igraph')
+
         # test infomap plotting with edges not shown
-        vis.plot_infomap_igraph(message_graph,membership, self.test_data_dir, "message", show_edges=False)
+        vis.plot_infomap_igraph(message_graph, membership, self.test_data_dir, "message", show_edges=False)
         self.assertTrue(mock_igraph_plot.call_args[0][0].isomorphic_vf2(igraph))
-        
+
         # check infomap generation with None membership
-        vis.plot_infomap_igraph(message_graph,None, self.test_data_dir, "message")
+        vis.plot_infomap_igraph(message_graph, None, self.test_data_dir, "message")
         self.assertTrue(mock_igraph_plot.call_args[0][0].isomorphic_vf2(igraph))
-        
-    @patch("lib.config.DEBUGGER",1)
-    @patch("igraph.plot",autospec=True)
-    def test_plot_infomap_igraph_show_edges(self,mock_igraph_plot):
-        message_graph = util.load_from_disk(self.test_data_dir+'/vis/message_graph')
-        membership = util.load_from_disk(self.test_data_dir+'/vis/membership')
-        igraph = util.load_from_disk(self.test_data_dir+'/vis/igraph')
-        
+
+    @patch("lib.config.DEBUGGER", 1)
+    @patch("igraph.plot", autospec=True)
+    def test_plot_infomap_igraph_show_edges(self, mock_igraph_plot):
+        message_graph = util.load_from_disk(self.test_data_dir + '/vis/message_graph')
+        membership = util.load_from_disk(self.test_data_dir + '/vis/membership')
+        igraph = util.load_from_disk(self.test_data_dir + '/vis/igraph')
+
         # test for infomap generation with edges
-        vis.plot_infomap_igraph(message_graph,membership, self.test_data_dir, "message", show_edges=True)
+        vis.plot_infomap_igraph(message_graph, membership, self.test_data_dir, "message", show_edges=True)
         self.assertTrue(mock_igraph_plot.call_args[0][0].isomorphic_vf2(igraph))
-        
-    @patch("lib.config.DEBUGGER",1)
-    @patch("igraph.plot",autospec=True)
-    def test_plot_infomap_igraph_aux_data(self,mock_igraph_plot):
-        message_graph = util.load_from_disk(self.test_data_dir+'vis/message_exchange_graph')
-        membership = util.load_from_disk(self.test_data_dir+'vis/message_exchange_graph_membership')
-        igraph = util.load_from_disk(self.test_data_dir+'vis/message_exchange_igraph')
-        aux = util.load_from_disk(self.test_data_dir+'vis/aux_data')
-        
+
+    @patch("lib.config.DEBUGGER", 1)
+    @patch("igraph.plot", autospec=True)
+    def test_plot_infomap_igraph_aux_data(self, mock_igraph_plot):
+        message_graph = util.load_from_disk(self.test_data_dir + 'vis/message_exchange_graph')
+        membership = util.load_from_disk(self.test_data_dir + 'vis/message_exchange_graph_membership')
+        igraph = util.load_from_disk(self.test_data_dir + 'vis/message_exchange_igraph')
+        aux = util.load_from_disk(self.test_data_dir + 'vis/aux_data')
+
         # test for infomap generation with auxiliary data
-        vis.plot_infomap_igraph(message_graph,membership, self.test_data_dir, "message", aux_data = aux)
+        vis.plot_infomap_igraph(message_graph, membership, self.test_data_dir, "message", aux_data=aux)
         self.assertTrue(mock_igraph_plot.call_args[0][0].isomorphic_vf2(igraph))
 
     @patch("lib.vis.calc_plot_linear_fit", autospec=True)
