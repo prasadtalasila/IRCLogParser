ce581a4702d98ed8da331f264bf46b6ebe13c30d
Kausam <Kausam@users.noreply.github.com>
2018-04-10 10:07:22 +0530

    Consolidate code coverage reports (#328)
    sends all code coverage reports to codecov

--- script/integration_test.sh
+++ script/integration_test.sh
@@ -2,4 +2,5 @@
 set -ex
 coverage run --source=. -m unittest discover -s test/integration-test
 coverage report -m
+codecov -F integration
 codeclimate-test-reporter --file .coverage --token $CODECLIMATE_REPO_TOKEN || :
