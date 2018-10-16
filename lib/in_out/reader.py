221f6b4fc444b9ea4487a4c18705a2365904cb6d
tejalwakchoure <tejal.wakchoure@gmail.com>
2018-10-16 21:39:51 +0530

    improve reader structure and add tests

--- lib/in_out/reader.py
+++ lib/in_out/reader.py
@@ -8,6 +8,7 @@ import collections
 from collections import defaultdict
 from dateutil.rrule import rrule, DAILY
 import logging
+import ConfigParser
 
 def linux_input(log_directory, channels_requested, starting_date, ending_date):
     """  
@@ -25,45 +26,103 @@ def linux_input(log_directory, channels_requested, starting_date, ending_date):
 
     """
     logs = defaultdict(list)
-    reader = UbuntuReader()
+    linuxreaderfactory = LinuxReaderFactory()
     
     #splitting starting date and ending date
     (starting_year, starting_month, starting_day) = [int(x) for x in starting_date.split('-')]
     (ending_year, ending_month, ending_day) = [int(x) for x in ending_date.split('-')]
-    
+
     start_date = datetime.date(starting_year, starting_month, starting_day)
     end_date = datetime.date(ending_year, ending_month, ending_day)
-    
-    for channel, date, log in reader.read_log(log_directory, channels_requested, start_date, end_date):
-        date_key = date
-        
-        value = {
-                "log_data": log,
-                "auxiliary_data": {
-                        "channel": channel,
-                        "year": date.year,
-                        "month": date.month,
-                        "day": date.day
-                    }
-            }
-        logs[date_key].append(value)
+
+    for channel_name in channels_requested:            
+        reader = linuxreaderfactory.reader_object(channel_name)
+        linux_input_logging(reader, log_directory, channel_name, start_date, end_date, logs);
         
     return collections.OrderedDict(sorted(logs.items()))
+	    	    
     
+def linux_input_logging(reader, log_directory, channel_name, start_date, end_date, logs):
+    
+        if reader == None:
+            logging.warn("Invalid channel name {}".format(channel_name))
+            return
+            
+        for channel, date, log in reader.read_log(log_directory, [channel_name], start_date, end_date):
+            date_key = date		
+            value = {
+		            "log_data": log,
+		            "auxiliary_data": {
+		                    "channel": channel,
+		                    "year": date.year,
+		                    "month": date.month,
+		                    "day": date.day
+		                }
+		        }
+            logs[date_key].append(value)
+
+class ReaderFactory:
+  
+    def reader_object_name(self, mapping):
+        pass
+         
+    def reader_object(self, channel_name):
+	    pass
+
+
+class MappingParser(ConfigParser.ConfigParser):
+    
+    def as_dict(self):        
+        d = dict(self._sections)
+        for k in d:
+            d[k] = dict(self._defaults, **d[k])
+            d[k].pop('__name__', None)
+        
+        return d
+
+
+class LinuxReaderFactory(ReaderFactory):
+
+    def reader_object_name(self, channel_name, mapping):        
+        reader_name = None
+        for key in mapping['Channels'].keys():                
+            if channel_name in mapping['Channels'][key].split(','):
+                reader_name = key
+                break
+        
+        return reader_name        
+        
+    def reader_object(self, channel_name):       
+        parser = MappingParser()
+        parser.read('channel_reader_mapping.ini')
+        mapping= parser.as_dict()
+        
+        reader = None
+        reader_name = self.reader_object_name(channel_name, mapping)
+        if reader_name == 'ubuntu':
+            reader = UbuntuReader()
+        elif reader_name == 'slack':
+            reader = SlackReader()
+        elif reader_name == 'scummvm':
+            reader = ScummVMReader()
+        
+        return reader
+
+
 class Reader:
+
     def __init(self):
         pass
        
     def _build_path(self, log_directory, current_date, channel):
-        path = "{}/{}/{}.txt".format(log_directory, current_date.strftime("%Y/%m/%d"), channel)
-        return path
-    
-    def _read_single_channel_log(self, log_directory, channel, start_date, end_date):
+        pass
+
+    def __read_single_channel_log(self, reader, log_directory, channel, start_date, end_date):
         one_day = datetime.timedelta(1)
         current_date = start_date
         
         while current_date <= end_date:
-            log_path = self._build_path(log_directory, current_date, channel)
+            log_path = reader._build_path(log_directory, current_date, channel)
             
             if not os.path.exists(log_path):
                 logging.warn("No Logs found at {}".format(log_path))
@@ -77,33 +136,45 @@ class Reader:
             
             current_date = current_date + one_day
         
-    def read_log(self, log_directory, channels, start_date, end_date):
-    
+    def __read_log(self, reader, log_directory, channels, start_date, end_date): 
         if len(channels) != 1 or "ALL" in channels:
             logging.warn("This Reader class doesnot support multi channel \
-                         log read. If you wish to read ubuntu logs use \
-                         UbuntuReader or else extend this class to create\
-                         a subclass with multi channel read support")
+                        log read. If you wish to read ubuntu logs use \
+                        UbuntuReader or else extend this class to create\
+                        a subclass with multi channel read support")
             return
             
-        return self._read_single_channel_log(log_directory, channels[0], start_date, end_date)  
-        
-       
+        return reader._read_single_channel_log(log_directory, channels[0], start_date, end_date)        
+    
 
 class UbuntuReader(Reader, object):
-    
-    def __init__(self):
-        super(UbuntuReader, self).__init__()
 
-    def _read_all_channel_logs(self, log_directory, start_date, end_date):
+    def __init__(self):
         pass
+        
+    def _build_path(self, log_directory, current_date, channel):
+        path = "{}/{}/{}.txt".format(log_directory, current_date.strftime("%Y/%m/%d"), channel)
+        return path    
+
+    def _read_single_channel_log(self, log_directory, channel, start_date, end_date):
+        return super(UbuntuReader, self)._Reader__read_single_channel_log(self, log_directory, channel, start_date, end_date)
               
     def _read_multi_channel_logs(self, log_directory, channels, start_date, end_date):
         for i in range(len(channels)):
             yield self._read_single_channel_log(log_directory, channels[i], start_date, end_date)
         
-    def read_log(self,log_directory, channels, start_date, end_date):
-       
+    def _read_all_channel_logs(self, log_directory, start_date, end_date):
+        
+        channels_requested=list()        
+        parser = MappingParser()
+        parser.read('channel_reader_mapping.ini')
+        mapping= parser.as_dict()
+        for channel_name in mapping['Channels']['ubuntu'].split(','):
+            channels_requested.append(channel_name)
+            
+        return self._read_multi_channel_logs(log_directory, channels_requested, start_date, end_date)        
+        
+    def read_log(self,log_directory, channels, start_date, end_date):       
         if len(channels) == 1 and channels[0] == "ALL":
             return self._read_all_channel_logs(log_directory, start_date, end_date)
         elif len(channels) == 1:
@@ -115,20 +186,30 @@ class UbuntuReader(Reader, object):
 class SlackReader(Reader, object):
 
     def __init__(self):
-        super(SlackReader, self).__init__()
-    
+       pass
+
     def _build_path(self, log_directory, current_date, channel):
         path = "{}/{}/{}.{}.log".format(log_directory, current_date.strftime("%Y"), channel,current_date.strftime("%Y%m%d"))
         return path
-        
-    def read_log(self, log_directory, start_date, end_date):
-        return super(SlackReader, self).read_log(log_directory, ["slackware"], start_date, end_date)
-        
+
+    def _read_single_channel_log(self, log_directory, channel, start_date, end_date): 
+        return super(SlackReader, self)._Reader__read_single_channel_log(self, log_directory, channel, start_date, end_date)
+                
+    def read_log(self, log_directory, channels, start_date, end_date):        
+        return super(SlackReader, self)._Reader__read_log(self, log_directory, channels, start_date, end_date)
+    
+    
 class ScummVMReader(Reader, object):
 
     def __init__(self):
-        super(ScummVMReader, self).__init__()
+        pass
         
-    def read_log(self, log_directory, start_date, end_date):
-        return super(ScummVMReader, self).read_log(log_directory, ["scummvm"], start_date, end_date)
+    def _build_path(self, log_directory, current_date, channel):
+        path = "{}/{}/{}.txt".format(log_directory, current_date.strftime("%Y/%m/%d"), channel)
+        return path  
 
+    def _read_single_channel_log(self, log_directory, channel, start_date, end_date):        
+        return super(ScummVMReader, self)._Reader__read_single_channel_log(self, log_directory, channel, start_date, end_date)
+            
+    def read_log(self, log_directory, channels, start_date, end_date):    
+        return super(ScummVMReader, self)._Reader__read_log(self, log_directory, channels, start_date, end_date)
--- test/unit-test/test_lib/test_in_out/test_reader.py
+++ test/unit-test/test_lib/test_in_out/test_reader.py
@@ -4,125 +4,193 @@ import datetime
 from datetime import datetime as dt
 from itertools import islice
 from lib.in_out import reader
-from lib.in_out.reader import Reader, UbuntuReader, SlackReader, ScummVMReader
+from lib.in_out.reader import ReaderFactory, LinuxReaderFactory, Reader, UbuntuReader, SlackReader, ScummVMReader
+
 
 class LinuxInputTest(unittest.TestCase):
 	
-	def test_linux_input(self):
+	def test_linux_input(self):	
 	
 		current_directory = os.path.dirname(os.path.realpath(__file__))
 		log_data = current_directory+ "/data/log"
 		start_date_str = "2013-1-1"
 		end_date_str = "2013-1-1"
-		channel_name = ["#kubuntu-devel"]
-		expected_output={'log_data': '=== Mamarok_ is now known as Mamarok\n', 'auxiliary_data': {'month': 1, 'day': 1, 'channel': '#kubuntu-devel', 'year': 2013}}
+		channel_name = ["#kubuntu-devel", "slackware", "scummvm", "invalid_channel"]
+		expected_output_ubuntu = {'log_data': '=== Mamarok_ is now known as Mamarok\n', 'auxiliary_data': {'month': 1, 'day': 1, 'channel': '#kubuntu-devel', 'year': 2013}}
+		expected_output_slack = {'log_data': '[00:07:50] <TheM4ch1n3> ah\n', 'auxiliary_data': {'month': 1, 'day': 1, 'channel': 'slackware', 'year': 2013}}
+		expected_output_scummvm = {'log_data': 'p0\n', 'auxiliary_data': {'month': 1, 'day': 1, 'channel': 'scummvm', 'year': 2013}}
 		
-		log_dir=reader.linux_input(log_data, channel_name, start_date_str, end_date_str)
-		self.assertEqual(log_dir.values()[0][10], expected_output)
-		
-		
+		log_dir = reader.linux_input(log_data, channel_name, start_date_str, end_date_str)
+		self.assertEqual(log_dir.values()[0][10], expected_output_ubuntu)
+		self.assertEqual(log_dir.values()[0][14], expected_output_slack)
+		self.assertEqual(log_dir.values()[0][34], expected_output_scummvm)
+
+class ReaderFactoryTest(unittest.TestCase):
+  
+    def test_reader_object(self):	    
+	    pass
+
+
+class LinuxReaderFactoryTest(ReaderFactoryTest, unittest.TestCase):
+
+    def test_reader_object(self):    
+        
+        linuxReader = LinuxReaderFactory()
+        self.assertIsInstance(linuxReader.reader_object("#kubuntu-devel"), UbuntuReader)
+        self.assertIsInstance(linuxReader.reader_object("slackware"), SlackReader)
+        self.assertIsInstance(linuxReader.reader_object("scummvm"), ScummVMReader)
+
+
 class ReaderTest(unittest.TestCase):
 
 	def setUp(self):
-	
+		
 		self.current_directory = os.path.dirname(os.path.realpath(__file__))
 		self.log_data = self.current_directory+ "/data/log"
 		self.start_date = dt.strptime("2013-1-1", '%Y-%m-%d')
 		self.end_date = dt.strptime("2013-1-1", '%Y-%m-%d')
-		self.channel_name = ["#kubuntu-devel"]
-		
+		self.channel_name = ["#kubuntu-devel", "slackware", "scummvm"]	
+			
 		self.start_date_2 = dt.strptime("2000-1-1", '%Y-%m-%d')
 		self.end_date_2 = dt.strptime("2000-1-1", '%Y-%m-%d')
-		self.expected_output1= ('#kubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== LordOfTime is now known as TheLordOfTime\n')
-		self.expected_output2= ('#kubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== Mamarok_ is now known as Mamarok\n')
-		self.expected_output3= ('#ubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== nerd is now known as superbot\n')
+		
+		self.ubuntu_expected_output_1 = ('#kubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== LordOfTime is now known as TheLordOfTime\n')
+		self.ubuntu_expected_output_2 = ('#kubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== Mamarok_ is now known as Mamarok\n')
+		self.ubuntu_expected_output_3 = ('#ubuntu-devel', datetime.datetime(2013, 1, 1, 0, 0), '=== nerd is now known as superbot\n')
+		
+		self.slack_expected_output = ('slackware', datetime.datetime(2013, 1, 1, 0, 0), '[00:06:41] Guest86829 (~alejandro@190.250.76.206) left irc: Quit: Lost terminal\n')
+		self.scummvm_expected_output = ('scummvm', datetime.datetime(2013, 1, 1, 0, 0), 'ccollections\n')
+		
+		self.ubuntuReader = UbuntuReader()
+		self.slackReader = SlackReader()
+		self.scummvmReader = ScummVMReader()
 
-	def tearDown(self):
+	def tearDown(self):	
 	
 		self.log_data = None
 		self.start_date = None
 		self.end_date = None
-		self.channel_name= None
+		self.channel_name= None		
 		
 		self.start_date_2 = None
 		self.end_date_2 = None
-		self.expected_output1= None
-		self.expected_output2= None
-		self.expected_output3= None
-
-	def test_build_path(self):
-	
-		path= Reader()._build_path(self.log_data, self.start_date, self.channel_name[0])
-		self.assertEqual(path, self.log_data+"/2013/01/01/#kubuntu-devel.txt")
-	
-	def test_read_single_channel_log(self):
 		
-		output= tuple(Reader()._read_single_channel_log(self.log_data, self.channel_name[0], self.start_date, self.end_date))		
-		self.assertEqual(output[0], self.expected_output1)
-		with self.assertRaises(IOError):
-			tuple(Reader()._read_single_channel_log(self.log_data, self.channel_name[0], self.start_date_2, self.end_date_2))
-
-	def test_read_log(self):
-	
-		output1= tuple(Reader().read_log(self.log_data, self.channel_name, self.start_date, self.end_date))
-		self.assertEqual(output1[0], self.expected_output1)
-		output2= Reader().read_log(self.log_data, self.channel_name+ ["#ubuntu-devel"], self.start_date, self.end_date)
-		output3= Reader().read_log(self.log_data, ["ALL"], self.start_date, self.end_date)
-		self.assertIsNone(output2)
-		self.assertIsNone(output3)
-			
-
-class UbuntuReaderTest(ReaderTest,unittest.TestCase):
-	
-	def test_read_multi_channel_logs(self):
-	
-		output= UbuntuReader()._read_multi_channel_logs(self.log_data, self.channel_name+ ["#ubuntu-devel"], self.start_date, self.end_date)		
-		output_to_check= []
-		for i in islice(output, 0, 2):
-			for j in islice(i, 10, 11):
-				output_to_check.append(j)		
+		self.ubuntu_expected_output_1 = None
+		self.ubuntu_expected_output_2 = None
+		self.ubuntu_expected_output_3 = None
 		
-		self.assertEqual(output_to_check[0], self.expected_output2)
-		self.assertEqual(output_to_check[1], self.expected_output3)
-	
-	def test_read_log(self):
+		self.slack_expected_output = None
+		self.scummvm_expected_output = None
 		
-		output1= UbuntuReader().read_log(self.log_data, ["ALL"], self.start_date, self.end_date)
-		output2= tuple(UbuntuReader().read_log(self.log_data, self.channel_name, self.start_date, self.end_date))
-		output3= UbuntuReader().read_log(self.log_data, self.channel_name+ ["#ubuntu-devel"], self.start_date, self.end_date)
-		output3_to_check=[]
-		for i in islice(output3, 0, 2):
-			for j in islice(i, 10, 11):
-				output3_to_check.append(j)
-				
-		self.assertIsNone(output1)
-		self.assertEqual(output2[0], self.expected_output1)
-		self.assertEqual(output3_to_check[0], self.expected_output2)
-		self.assertEqual(output3_to_check[1], self.expected_output3)
+		self.ubuntuReader = None
+		self.slackReader = None
+		self.scummvmReader = None
+
+	def test_build_path(self):	    
+	    pass
+	    
+	def test_read_single_channel_log(self):		
+		pass
+		
+	def test_read_log(self):	
+		pass
+					
+
+class UbuntuReaderTest(ReaderTest, unittest.TestCase):
+	
+    def test_build_path(self):
+        path = self.ubuntuReader._build_path(self.log_data, self.start_date, self.channel_name[0])
+        self.assertEqual(path, self.log_data+"/2013/01/01/#kubuntu-devel.txt")
+        
+    def test_read_single_channel_log(self):	
+    	
+        output = tuple(self.ubuntuReader._read_single_channel_log(self.log_data, self.channel_name[0], self.start_date, self.end_date))		
+        self.assertEqual(output[0], self.ubuntu_expected_output_1)
+        with self.assertRaises(IOError):
+            tuple(self.ubuntuReader._read_single_channel_log(self.log_data, self.channel_name[0], self.start_date_2, self.end_date_2))           
+        
+    def test_read_multi_channel_logs(self):	
+    
+        output = self.ubuntuReader._read_multi_channel_logs(self.log_data, [self.channel_name[0], "#ubuntu-devel"], self.start_date, self.end_date)		
+        output_to_check = []
+        for i in islice(output, 0, 2):
+            for j in islice(i, 10, 11):
+                output_to_check.append(j)		
+		
+        self.assertEqual(output_to_check[0], self.ubuntu_expected_output_2)
+        self.assertEqual(output_to_check[1], self.ubuntu_expected_output_3)
+        
+    def test_read_all_channel_logs(self):
+         output = self.ubuntuReader._read_all_channel_logs(self.log_data, self.start_date, self.end_date)
+         output_to_check = []
+         for i in islice(output, 19, 20):
+            for j in islice(i, 0, 1):
+                output_to_check.append(j)
+         self.assertEqual(output_to_check[0], self.ubuntu_expected_output_1)
+    	
+    def test_read_log(self):
+		        
+        output1 = self.ubuntuReader.read_log(self.log_data, ["ALL"], self.start_date, self.end_date)
+        output2 = tuple(self.ubuntuReader.read_log(self.log_data, [self.channel_name[0]], self.start_date, self.end_date))
+        output3 = self.ubuntuReader.read_log(self.log_data, [self.channel_name[0], "#ubuntu-devel"], self.start_date, self.end_date)
+        output1_to_check = []
+        output3_to_check = []
+        
+        for i in islice(output1, 19, 20):
+            for j in islice(i, 0, 1):
+                output1_to_check.append(j)        
+        for i in islice(output3, 0, 2):
+            for j in islice(i, 10, 11):
+                output3_to_check.append(j)		
+        
+        self.assertEqual(output1_to_check[0], self.ubuntu_expected_output_1)
+        self.assertEqual(output2[0], self.ubuntu_expected_output_1)
+        self.assertEqual(output3_to_check[0], self.ubuntu_expected_output_2)
+        self.assertEqual(output3_to_check[1], self.ubuntu_expected_output_3)
 
 		
-class SlackReaderTest(ReaderTest,unittest.TestCase):
-
-	def test_build_path(self):
+class SlackReaderTest(ReaderTest, unittest.TestCase):    
+    
+    def test_build_path(self):
 	
-		path= SlackReader()._build_path(self.log_data, self.start_date, self.channel_name[0])
-		self.assertEqual(path, self.log_data+"/2013/#kubuntu-devel.20130101.log")
+        path = self.slackReader._build_path(self.log_data, self.start_date, self.channel_name[1])
+        self.assertEqual(path, self.log_data+"/2013/slackware.20130101.log")
 		
-	def test_read_log(self):
+    def test_read_single_channel_log(self):
+		
+        output = tuple(self.slackReader._read_single_channel_log(self.log_data, self.channel_name[1], self.start_date, self.end_date))		
+        self.assertEqual(output[0], self.slack_expected_output)
+        with self.assertRaises(IOError):
+            tuple(self.slackReader._read_single_channel_log(self.log_data, self.channel_name[1], self.start_date_2, self.end_date_2))       	
+		
+    def test_read_log(self):
 	
-		self.assertIsNotNone(tuple(SlackReader().read_log(self.log_data, self.start_date, self.end_date)))
-		with self.assertRaises(IOError):
-			tuple(SlackReader().read_log(self.log_data, self.start_date_2, self.end_date_2))
+        self.assertIsNotNone(tuple(self.slackReader.read_log(self.log_data, [self.channel_name[1]], self.start_date, self.end_date)))
+        with self.assertRaises(IOError):
+            tuple(self.slackReader.read_log(self.log_data, [self.channel_name[1]], self.start_date_2, self.end_date_2))
 
 
-class TestScummVMReader(ReaderTest, unittest.TestCase):
-
-	def test_read_log(self):
+class ScummVMReaderTest(ReaderTest, unittest.TestCase):
+    
+    def test_build_path(self):
 	
-		self.assertIsNotNone(tuple(ScummVMReader().read_log(self.log_data, self.start_date, self.end_date)))
-		with self.assertRaises(IOError):
-			tuple(ScummVMReader().read_log(self.log_data, self.start_date_2, self.end_date_2))
+        path = self.scummvmReader._build_path(self.log_data, self.start_date, self.channel_name[2])
+        self.assertEqual(path, self.log_data+"/2013/01/01/scummvm.txt")
+		
+    def test_read_single_channel_log(self):
+		
+        output = tuple(self.scummvmReader._read_single_channel_log(self.log_data, self.channel_name[2], self.start_date, self.end_date))		
+        self.assertEqual(output[0], self.scummvm_expected_output)
+        with self.assertRaises(IOError):
+            tuple(self.scummvmReader._read_single_channel_log(self.log_data, self.channel_name[2], self.start_date_2, self.end_date_2)) 
+
+    def test_read_log(self):
+	
+        self.assertIsNotNone(tuple(self.scummvmReader.read_log(self.log_data, [self.channel_name[2]], self.start_date, self.end_date)))
+        with self.assertRaises(IOError):
+            tuple(self.scummvmReader.read_log(self.log_data, [self.channel_name[2]], self.start_date_2, self.end_date_2))
 
 				
 if __name__ == '__main__':
     unittest.main()
+    
