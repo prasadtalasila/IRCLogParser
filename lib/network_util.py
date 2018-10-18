5802da6bf0d15235e7812c1af0f33bac8cc8db0b
hrishikesh-dahiya <f20160057@goa.bits-pilani.ac.in>
2018-10-18 11:03:54 +0530

    Add test for network_util.py

--- lib/network_util.py
+++ lib/network_util.py
@@ -6,10 +6,105 @@ used in IRCLogParser from NetworkX and iGraph
 import networkx as nx 
 
 
-class Graph(object):
+class Graph():
+
+    node_dict_factory = dict
+    adjlist_outer_dict_factory = dict
+    adjlist_inner_dict_factory = dict
+    edge_attr_dict_factory = dict
+
+
+    def to_directed_class(self):
+        return nx.DiGraph
+
+    def to_undirected_class(self):
+        return nx.Graph
 
     def __init__(self):
         self.graph = nx.Graph()
+        self.graph.__init__()
+
+    @property
+    def adj(self):
+        return self.graph.adj
+
+    @property
+    def name(self):
+        return self.graph.name
+
+    @name.setter
+    def name(self, s):
+        self.name = s       
+
+    def __str__(self):
+        return self.name
+
+    def __iter__(self):
+        return self.graph.__iter__()
+
+    def __contains__(self, n):
+        return self.graph.__contains__(n)
+
+    def __len__(self):
+        return self.graph.__len__()
+
+    def __getitem__(self, n):
+        self.graph.__getitem__(n)
+
+    def add_node(self, node_for_adding):
+        self.graph.add_node(node_for_adding)
+
+    def add_nodes_from(self, nodes_for_adding, **attr):
+        self.graph.add_nodes_from(nodes_for_adding)
+
+    def remove_node(self, n):
+        self.graph.remove_node(n)
+
+    def remove_nodes_from(self, nodes):
+        self.graph.remove_nodes_from(nodes)
+
+    @property
+    def nodes(self):
+        return self.graph.nodes()
+
+    def number_of_nodes(self):
+        return self.graph.number_of_nodes()
+
+    def had_node(self, n):
+        return self.graph.has_node(n)
+
+    def add_edge(self, u_of_edge, v_of_edge, **attr):
+        self.graph.add_edge(u_of_edge, v_of_edge)
+
+    def add_edges_from(self, ebunch_to_add, **attr):
+        self.graph.add_edges_from(ebunch_to_add)
+
+    def is_directed(self):
+        return self.graph.is_directed()
+
+    def is_multigraph(self):
+        return self.graph.is_multigraph()
+
+    def update(self, edges=None, nodes=None):
+        # self.graph.update(edges=edges, nodes=nodes)
+        pass
+
+    def edges(self):
+        return self.graph.edges()
+
+    def has_edge(self, u, v):
+        return self.graph.has_edge(u, v)
+
+    def neighbors(self, n):
+        return self.graph.neighbors(n)
+
+    def adjacency(self):
+        # return self.graph.adjacency()
+        pass
+
+    @property
+    def degree(self):
+        return self.graph.degree()    
 
 
 class DiGraph(Graph):
@@ -17,11 +112,47 @@ class DiGraph(Graph):
     def __init__(self):
         self.digraph = nx.DiGraph()
 
+    def add_edge(self, u_of_edge, v_of_edge, **attr):
+        self.digraph.add_edge(u_of_edge, v_of_edge)
+
+    def add_nodes_from(self, nodes_for_adding, **attr):
+        self.digraph.add_nodes_from(nodes_for_adding)
+
+    def is_directed(self):
+        self.digraph.is_directed()
+
+    def out_degree(self):
+        self.digraph.out_degree()
+
+    @property
+    def nodes(self):
+        self.digraph.nodes()
+
+    @property
+    def edges(self):
+        self.digraph.edges()
+
+    @property
+    def name(self):
+        self.digraph.name
+
+
+class MultiGraph(Graph):
+
+    def __init__(self):
+        self.multigraph = nx.MultiGraph()
+
 
 class MultiDiGraph(MultiGraph, DiGraph):
 
     def __init__(self):
         self.multidigraph = nx.MultiDiGraph()
+
+    def add_edge(self, u_for_edge, v_for_edge, key=None, **attr):
+        self.multidigraph.add_edge(u_for_edge, v_for_edge)
+
+    def is_directed(self):
+        self.multidigraph.is_directed()
 
 
 def read_pajek(path, encoding='UTF-8'):
@@ -31,7 +162,7 @@ def read_pajek(path, encoding='UTF-8'):
 
 def is_isomorphic(G1, G2, node_match=None, edge_match=None):
 
-    return nx.algorithms.isomorphism.is_isomorphic(G1, G2, node_match, edge_match)
+    return nx.algorithms.isomorphism.is_isomorphic(G1, G2)
 
 
 def to_agraph(N):
@@ -46,19 +177,19 @@ def connected_components(G):
 
 def relabel_nodes(G, mapping, copy=True):
 
-    return nx.relabel.relabel_nodes(G, mapping, copy)
+    return nx.relabel.relabel_nodes(G, mapping)
 
 
 def write_pajek(G, path, encoding='UTF-8'):
 
-    return nx.readwrite.pajek.write_pajek(G, path, encoding)
+    return nx.readwrite.pajek.write_pajek(G, path)
 
 
 def hits(G, max_iter=100, tol=1.0e-8, nstart=None, normalized=True):
 
-    return nx.hits(G, max_iter, tol, nstart, normalized)
+    return nx.hits(G)
 
 
 def node_link_data(G, attrs=None):
 
-    return nx.readwrite.json_graph.node_link_data(G, attrs)
+    return nx.readwrite.json_graph.node_link_data(G)
---
+++ test/integration-test/test_lib/test_network_util.py
@@ -0,0 +1,91 @@
+import lib.network_util as network_util
+import networkx as nx
+import unittest
+import os
+import sys
+
+
+class NetworkUtilTest(unittest.TestCase):
+
+    def setUp(self):
+        self.current_directory = os.path.dirname(os.path.realpath(__file__))
+
+    def tearDown(self):
+        self.current_directory = None
+
+    def test_read_pajek(self):
+        expected_graph = nx.read_pajek(self.current_directory + "/data/message_number_graph.net")
+        output_graph = network_util.read_pajek(self.current_directory + "/data/message_number_graph.net")
+
+        self.assertTrue(nx.is_isomorphic(expected_graph, output_graph))
+
+    def test_is_isomorphic(self):
+        graph = nx.read_pajek(self.current_directory + "/data/message_number_graph.net")
+
+        self.assertTrue(network_util.is_isomorphic(graph, graph))
+
+    def test_to_agraph(self):
+        graph = nx.read_pajek(self.current_directory + "/data/message_number_graph.net")
+
+        expected_agraph = nx.nx_agraph.to_agraph(graph)
+        output_agraph = network_util.to_agraph(graph)
+
+        self.assertEquals(expected_agraph, output_agraph)
+
+    def test_connected_components(self):
+        graph = nx.path_graph(4)
+        graph.add_path([10, 11, 12])
+
+        expected_generator = nx.connected_components(graph)
+        output_generator = network_util.connected_components(graph)
+
+        self.assertEquals(list(expected_generator), list(output_generator))
+
+    def test_relabel_nodes(self):
+        graph = nx.path_graph(3)
+
+        mapping = {0: 'a', 1: 'b', 2: 'c'}
+
+        self.assertEquals(sorted(nx.relabel_nodes(graph, mapping)), sorted(network_util.relabel_nodes(graph, mapping)))
+
+    def test_write_pajek(self):
+        graph = nx.path_graph(3)
+
+        path = self.current_directory + "/data/test_write_pajek_output.net"
+
+        network_util.write_pajek(graph, path)
+
+        read_graph = nx.read_pajek(path)
+
+        self.assertTrue(network_util.is_isomorphic(graph, read_graph))
+
+    def test_hits(self):
+        graph = nx.path_graph(4)
+
+        expected_tuple1, expected_tuple2 = nx.hits(graph)
+
+        output_tuple1, output_tuple2 = network_util.hits(graph)
+
+        self.assertEquals(expected_tuple1, output_tuple1)
+        self.assertEquals(expected_tuple2, output_tuple2)
+
+    def node_link_data(self):
+        graph = nx.path_graph(4)
+
+        expected_dict = nx.readwrite.json_graph.node_link_data(graph)
+        output_dict = network_util.node_link_data(graph)
+
+        self.assertEquals(expected_dict, output_dict)
+
+
+    def test_Graph(self):
+        graph = network_util.Graph()
+        
+        graph.add_node(1)
+
+        graph.name('s')
+
+        self.assertEquals(graph.name, 'g')
+
+if __name__ == '__main__':
+    unittest.main()
