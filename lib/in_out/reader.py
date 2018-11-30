46df6cdf9bcee16f40f32d5c71a74833f35e5540
tejalwakchoure <35342471+tejalwakchoure@users.noreply.github.com>
2018-11-30 22:30:03 +0530

    Update reader.py

--- lib/in_out/reader.py
+++ lib/in_out/reader.py
@@ -8,7 +8,6 @@ import collections
 from collections import defaultdict
 from dateutil.rrule import rrule, DAILY
 import logging
-import ConfigParser
 
 def linux_input(log_directory, channels_requested, starting_date, ending_date):
     """  
@@ -69,7 +68,7 @@ def linux_input_logging(reader, logging_data, logs):
 
 class ReaderFactory:
   
-    def reader_object_name(self, mapping):
+    def reader_object_name(self):
         pass
          
     def reader_object(self, channel_name):
@@ -78,28 +77,25 @@ class ReaderFactory:
 
 class LinuxReaderFactory(ReaderFactory):
 
-    def reader_object_name(self, channel_name, mapping):        
+    def reader_object_name(self, channel_name):        
 
+        mapping = {'Ubuntu' : '#ubuntu-cat,#ubuntu-es,#ubuntu,#xubuntu,#ubuntu-discuss,#ubuntustudio,#ubuntu-beginners,#lubuntu,#ubuntustudio-devel,#ubuntu-irc,#ubuntu-us-nc,#ubuntu-news,#kubuntu,#ubuntu-motu,#ubuntu-ops,#ubuntu-server,#ubuntu-x,#ubuntu+1,#ubuntu-devel,#kubuntu-devel,#ubuntu-meeting,#ubuntu-translators,#edubuntu,#xubuntu-devel,#ubuntu-us-ca,#ubuntu-ke,#upstart,#ubuntu-kernel,#ubuntu-us-or,#ubuntu-community-team,#ubuntu-quality,#ubuntu-youth,#ubuntu-us-oh,#ubuntu-us-tn,#launchpad,#ubuntu-classroom,#ubuntu-us-pa,#ubuntu-gnome,#ubuntu-gnome-devel,#ubuntu-release,#ubuntu-bugs,#ubuntu-phone,#ubuntu-pl,#ubuntu-app-devel,#ubuntu-uk,#ubuntu-arb,#ubuntu-locoteams,#ubuntu-au,#ubuntu-leadership,#ubuntu-website,#ubuntu-doc,#ubuntu-manual,#ubuntu-learning,#ubuntu-on-air,#ubuntu-nl-mwanzo,#ubuntuone,#bzr,#launchpad-meeting,#launchpad-dev,#juju-dev,#ubuntu-de,#ubuntu-ru,#ubuntu-cn,#ubuntu-mythtv,#ubuntu-ro,#ubuntu-it,#ubuntu-vn,#ubuntu-br,#ubuntu-women-project,#ubuntu-nl,#ubuntu-design,#ubuntu-ie,#ubuntu-desktop,#ubuntu-arm,#maas,#ubuntu-nz,#ubuntu-za,#ubuntu-fi,#ubuntu-mozillateam,#ubuntu-qc,#ubuntu-ca,#ubuntu-pe,#ubuntu-us-mi,#ubuntu-uy,#ubuntu-unity,#ubuntu-dk-moede,#ubuntu-dk,#ubuntu-no,#ubuntu-ph,#ubuntu-my,#ubuntu-cm,#ubuntu-rs,#ubuntu-mk,#ubuntu-tablet,#ubuntu-tv,#ubuntu-it-meeting,#ubuntu-hr,#ubuntu-tr,#ubuntu-accessibility,#ubuntu-se,#ubuntu-ir,#ubuntu-si,#ubuntu-ko,#ubuntu-installer,#ubuntu-cl,#kubuntu-se,#ubuntu-cym,#ubuntu-eg,#ubuntu-tn,#ubuntu-cz,#ubuntu-dz,#ubuntu-us-wa,#ubuntu-ngo,#juju-gui,#ubuntu-classroom-es,#ubuntu-co,#ubuntu-us-ga,#ubuntu-jp,#launchpad-yellow,#ubuntu-papercuts,#ubuntu-us-co,#ubuntu-us-mn,#ubuntu-co-meeting,#ubuntu-ar,#ubuntu-es-locos,#ubuntu-us-fl,#ubuntu-touch,#ubuntu-us-dc,#ubuntu-us-tx,#ubuntu-sa,#ubuntu-bd,#ubuntu-artwork,#ubuntu-il,#ubuntu-fr-l10n,#ubuntu-toolchain,#ubuntu-ports,#ubuntu-zh,#ubuntu-java,#ubuntu-marketing,#ubuntu-us-sc,#ubuntu-eu,#ubuntu-tw,#ubuntu-laptop,#ubuntu-tam,#ubuntu-hk,#ubuntu-pk,#ubuntu-centroamerica,#ubuntu-charlas,#ubuntu-l10n-es,#launchpad-reviews,#ubuntu-us-ar,#ubuntu-cy,#ubuntu-lb,#ubuntu-tour,#ubuntu-for-all,#ubuntu-se-mote,#ubuntu-translators-ru,#ubuntu-pa,#ubuntu-hn,#ubuntu-us-me,#ubuntu-us-md,#ubuntu-sv,#ubuntu-tn-classroom,#ubuntu-tn-meeting', 'Slack' : 'slackware', 'ScummVM' : 'scummvm'}
         reader_name = None
-        for key,value in mapping:  
-            if channel_name in value.split(','):
+        for key in mapping.keys():  
+            if channel_name in mapping[key].split(','):
                 reader_name = key
                 break        
         return reader_name        
         
     def reader_object(self, channel_name):
 
-        parser = ConfigParser.ConfigParser()
-        parser.read(os.path.join(os.getcwd(),'channel_reader_mapping.ini'))
-        mapping = parser.items('Channels')
         reader = None
-        reader_name = self.reader_object_name(channel_name, mapping)
-        
-        if reader_name == 'ubuntu':
+        reader_name = self.reader_object_name(channel_name)
+        if reader_name == 'Ubuntu':
             reader = UbuntuReader()
-        elif reader_name == 'slack':
+        elif reader_name == 'Slack':
             reader = SlackReader()
-        elif reader_name == 'scummvm':
+        elif reader_name == 'ScummVM':
             reader = ScummVMReader()
         
         return reader
@@ -173,11 +169,9 @@ class UbuntuReader(Reader, object):
         
     def _read_all_channel_logs(self, log_directory, start_date, end_date):   
 
-        channels_requested = list() 
-        parser = ConfigParser.ConfigParser()
-        parser.read(os.path.join(os.getcwd(),'channel_reader_mapping.ini'))
-        mapping = parser.items('Channels')
-        for channel_name in mapping[0][1].split(','):
+        channels_requested = list()
+        ubuntu_mapping = {'Ubuntu' : '#ubuntu-cat,#ubuntu-es,#ubuntu,#xubuntu,#ubuntu-discuss,#ubuntustudio,#ubuntu-beginners,#lubuntu,#ubuntustudio-devel,#ubuntu-irc,#ubuntu-us-nc,#ubuntu-news,#kubuntu,#ubuntu-motu,#ubuntu-ops,#ubuntu-server,#ubuntu-x,#ubuntu+1,#ubuntu-devel,#kubuntu-devel,#ubuntu-meeting,#ubuntu-translators,#edubuntu,#xubuntu-devel,#ubuntu-us-ca,#ubuntu-ke,#upstart,#ubuntu-kernel,#ubuntu-us-or,#ubuntu-community-team,#ubuntu-quality,#ubuntu-youth,#ubuntu-us-oh,#ubuntu-us-tn,#launchpad,#ubuntu-classroom,#ubuntu-us-pa,#ubuntu-gnome,#ubuntu-gnome-devel,#ubuntu-release,#ubuntu-bugs,#ubuntu-phone,#ubuntu-pl,#ubuntu-app-devel,#ubuntu-uk,#ubuntu-arb,#ubuntu-locoteams,#ubuntu-au,#ubuntu-leadership,#ubuntu-website,#ubuntu-doc,#ubuntu-manual,#ubuntu-learning,#ubuntu-on-air,#ubuntu-nl-mwanzo,#ubuntuone,#bzr,#launchpad-meeting,#launchpad-dev,#juju-dev,#ubuntu-de,#ubuntu-ru,#ubuntu-cn,#ubuntu-mythtv,#ubuntu-ro,#ubuntu-it,#ubuntu-vn,#ubuntu-br,#ubuntu-women-project,#ubuntu-nl,#ubuntu-design,#ubuntu-ie,#ubuntu-desktop,#ubuntu-arm,#maas,#ubuntu-nz,#ubuntu-za,#ubuntu-fi,#ubuntu-mozillateam,#ubuntu-qc,#ubuntu-ca,#ubuntu-pe,#ubuntu-us-mi,#ubuntu-uy,#ubuntu-unity,#ubuntu-dk-moede,#ubuntu-dk,#ubuntu-no,#ubuntu-ph,#ubuntu-my,#ubuntu-cm,#ubuntu-rs,#ubuntu-mk,#ubuntu-tablet,#ubuntu-tv,#ubuntu-it-meeting,#ubuntu-hr,#ubuntu-tr,#ubuntu-accessibility,#ubuntu-se,#ubuntu-ir,#ubuntu-si,#ubuntu-ko,#ubuntu-installer,#ubuntu-cl,#kubuntu-se,#ubuntu-cym,#ubuntu-eg,#ubuntu-tn,#ubuntu-cz,#ubuntu-dz,#ubuntu-us-wa,#ubuntu-ngo,#juju-gui,#ubuntu-classroom-es,#ubuntu-co,#ubuntu-us-ga,#ubuntu-jp,#launchpad-yellow,#ubuntu-papercuts,#ubuntu-us-co,#ubuntu-us-mn,#ubuntu-co-meeting,#ubuntu-ar,#ubuntu-es-locos,#ubuntu-us-fl,#ubuntu-touch,#ubuntu-us-dc,#ubuntu-us-tx,#ubuntu-sa,#ubuntu-bd,#ubuntu-artwork,#ubuntu-il,#ubuntu-fr-l10n,#ubuntu-toolchain,#ubuntu-ports,#ubuntu-zh,#ubuntu-java,#ubuntu-marketing,#ubuntu-us-sc,#ubuntu-eu,#ubuntu-tw,#ubuntu-laptop,#ubuntu-tam,#ubuntu-hk,#ubuntu-pk,#ubuntu-centroamerica,#ubuntu-charlas,#ubuntu-l10n-es,#launchpad-reviews,#ubuntu-us-ar,#ubuntu-cy,#ubuntu-lb,#ubuntu-tour,#ubuntu-for-all,#ubuntu-se-mote,#ubuntu-translators-ru,#ubuntu-pa,#ubuntu-hn,#ubuntu-us-me,#ubuntu-us-md,#ubuntu-sv,#ubuntu-tn-classroom,#ubuntu-tn-meeting'}
+        for channel_name in ubuntu_mapping['Ubuntu'].split(','):
             channels_requested.append(channel_name)
             
         return self._read_multi_channel_logs(log_directory, channels_requested, start_date, end_date)        
